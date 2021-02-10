1f7f293c08765c811ce1ebe06348c7ee88b9ef4f
dogdduddy <dogdduddy@gmail.com>
2021-02-10 09:27:09 +0900

    good

--- app/src/main/java/com/example/jeogeum/MainActivity.java
+++ app/src/main/java/com/example/jeogeum/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.jeogeum;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,123 +18,53 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
-    private FirebaseAuth mAuth;
-    EditText etId, etPassword;
-    ProgressBar progress;
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
-        // 입력 된 아이디, 비밀번호
-        etId = (EditText)findViewById(R.id.etId);
-        etPassword = (EditText)findViewById(R.id.etPassWord);
-        // 로딩
-        progress = (ProgressBar)findViewById(R.id.progress);
-        // 로그인 버튼
-        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String stEmail = etId.getText().toString();
-                String stPassword = etPassword.getText().toString();
-                // Email, Password 입력 여부 파악
-                if(stEmail.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Email", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(stPassword.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                progress.setVisibility(View.VISIBLE);
-                // 로그인 기능 구현
-                mAuth.signInWithEmailAndPassword(stEmail, stPassword)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progress.setVisibility(View.INVISIBLE);
-                                if (task.isSuccessful()) {
-                                    // 성공
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    // Email, name 확인
-                                    String stUserEmail = user.getEmail();
-                                    String stUserName = user.getDisplayName();
-                                    Log.d(TAG, "stUSerEmail: "+stUserEmail+", stUserName: "+stUserName);
-                                    // 다음 화면으로 전환
-                                    Intent in = new Intent(MainActivity.this, ContentActivity.class);
-                                    in.putExtra("email", stEmail);
-                                    startActivity(in);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
 
-                                } else {
-                                    // 실패
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(MainActivity.this, "Login failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
-        // 회원 입력 정보 저장 버튼 ( Id, Password )
-        Button btnRegister = (Button)findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String stEmail = etId.getText().toString();
-                String stPassword = etPassword.getText().toString();
-                // Email, Password 입력 여부 파악
-                if(stEmail.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Email", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(stPassword.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                progress.setVisibility(View.VISIBLE);
-                // 회원가입 기능 구현
-                mAuth.createUserWithEmailAndPassword(stEmail, stPassword)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progress.setVisibility(View.INVISIBLE);
-                                if (task.isSuccessful()) {
-                                    // 성공
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                } else {
-                                    // 실패
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(MainActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                                // ...
-                            }
-                        });
-            }
-        });
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
-
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+
 }
