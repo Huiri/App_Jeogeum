2dc92bd1928ac5fa4178028368ee410a738970a4
Huiri <gmlfltj@naver.com>
2021-03-17 01:00:46 +0900

    실행 안됩니다요

--- app/src/main/java/com/example/jeogeum/Fragment3.java
+++ app/src/main/java/com/example/jeogeum/Fragment3.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,12 +12,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class Fragment3 extends Fragment {
+import java.util.List;
+import java.util.Objects;
 
+public class Fragment3 extends Fragment implements View.OnClickListener {
+
+    private Button Close_btn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return (ViewGroup) inflater.inflate(R.layout.fragment_3, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+        Close_btn = (Button) v.findViewById(R.id.finish);
+        Close_btn.setOnClickListener(this);
+        switch (v.getId()) {
+            case R.id.finish:
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction().remove(Fragment3.this).commit();
+                fragmentManager.popBackStack();
+        }
+        //실행 X
+    }
 }
